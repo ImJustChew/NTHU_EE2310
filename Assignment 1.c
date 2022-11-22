2d8b637a9582ea6256428d010e785da78d3e8b59
ImJustChew <chewtzihwee@gmail.com>
2022-11-23 00:35:16 +0800

    fixed not popping after push and save sol to str

--- Assignment 1.c
+++ Assignment 1.c
@@ -16,6 +16,12 @@ typedef struct stack {
     int max_size;
 } stack_arr_t;
 
+typedef struct sol_stack {
+    char **head;
+    int top;
+    int max_size;
+} sol_stack_t;
+
 
 void stack_init(stack_arr_t *s, int size) {
 	s->head = (coord_t *)malloc(sizeof(coord_t) * (size + 1));
@@ -76,22 +82,23 @@ void maze_init(maze_t *maze, int m, int n) {
 	}
 }
 
-void print(maze_t *maze, stack_arr_t *s) {
-	int i;
-	for (i = 0; i < s->top; i++) {
-		printf("(%d, %d)", s->head[i].x+1, maze->m - s->head[i].y);
+void sprint_stack(char *string, maze_t *maze, stack_arr_t *s) {
+	for (int i = 0; i < s->top; i++) {
+		sprintf(string + strlen(string), "(%d, %d)", s->head[i].x+1, maze->m - s->head[i].y);
 		if(i != s->top - 1) {
-			printf("->");
+			sprintf(string + strlen(string),"->");
 		}
 	}
-	printf("\n");
 }
 int next(maze_t *maze, stack_arr_t *l, int pos_x, int pos_y) {
 	// printf("(%d, %d) - (%d, %d) - %c\n", pos_x+1, maze->m - pos_y, pos_x, pos_y, maze->maze[pos_y][pos_x]);
 	// printf("top: %d, %d\n", top(l).x, top(l).y);
 	if(pos_x == (maze->n - 1) && pos_y == 0) {
+		char *sol = (char*)calloc(l->top * (2 + 2 + 2 + maze->m + maze->n), sizeof(char)); //one coord is at most "(m, n)->" long, which is 2+2+2+digits of n, digits of m
 		push(pos_x, pos_y, l);
-		print(maze, l);
+		sprint_stack(sol, maze, l);
+		printf("%s\n", sol);
+		pop(l);
 	}
 	else {
 		if((pos_x - 1) >= 0 && maze->maze[pos_y][pos_x - 1] == '0' && !coord_exists(l, pos_x - 1, pos_y)) {
@@ -125,14 +132,11 @@ int main(void) {
 	for (int i = 0; i < m; i++) {
 		for (int j = 0; j < n; j++) {
 			char temp;	
-			do {
-				scanf("%c", &temp);
-			}
+			do scanf("%c", &temp);
 			while(temp == '\n');
 			maze->maze[i][j] = temp;
 		}
 	}
-
 	stack_arr_t *list = (stack_arr_t*)malloc(sizeof(stack_arr_t));
 	stack_init(list, m*n);
 	next(maze, list, 0, m - 1);

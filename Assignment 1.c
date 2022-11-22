d3f4af15cad7a1a21408d741c834bb3ace20c98c
ImJustChew <chewtzihwee@gmail.com>
2022-11-23 00:12:26 +0800

    Assignment 1 works now

--- Assignment 1.c
+++ Assignment 1.c
@@ -6,15 +6,19 @@ typedef struct maze {
 	int m, n;
 } maze_t;
 
+typedef struct coord {
+	int x, y;
+} coord_t;
+
 typedef struct stack {
-    int *head;
+    coord_t *head;
     int top;
     int max_size;
 } stack_arr_t;
 
 
 void stack_init(stack_arr_t *s, int size) {
-	s->head = (int *)malloc(sizeof(int) * (size + 1));
+	s->head = (coord_t *)malloc(sizeof(coord_t) * (size + 1));
 	s->top = 0;
 	s->max_size = size;
 }
@@ -23,55 +27,114 @@ void stack_destroy(stack_arr_t *s) {
 	free(s->head);
 }
 
-void push(int elem, stack_arr_t *s) {
-	s->head[s->top++] = elem;
-
+void push(int x, int y, stack_arr_t *s) {
+	s->head[s->top].x = x;
+	s->head[s->top].y = y;
+	s->top++;
 }
 
-int pop(stack_arr_t *s) {
+coord_t top(stack_arr_t *s) {
 	if (s->top == 0) {
-		return MAX_NEG;
+		coord_t temp;
+		temp.x = -1;
+		temp.y = -1;
+		return temp;
+	
+	} else {
+		return s->head[s->top - 1];
+	}
+}
+
+coord_t pop(stack_arr_t *s) {
+	if (s->top == 0) {
+		coord_t temp;
+		temp.x = -1;
+		temp.y = -1;
+		return temp;
 	
 	} else {
 		return s->head[--s->top];
 	}
 }
-void print(list_t *l) {
-	node_t *p = l->head;
-	while (p!= NULL) {
-		printf("(%d,%d)", p->x, p->y);
-		if(p->next!= NULL) printf("->");
-		p = p->next;
+
+int coord_exists(stack_arr_t *s, int x, int y) {
+	int i;
+	for (i = 0; i < s->top; i++) {
+		if (s->head[i].x == x && s->head[i].y == y) {
+			return 1;
+		}
+	}
+	return 0;
+}
+
+void maze_init(maze_t *maze, int m, int n) {
+	maze->n = n;
+	maze->m = m;
+	maze->maze = (char**)malloc((m + 1) * sizeof(char*));
+	for (int i = 0; i < m; i++) {
+		maze->maze[i] = (char*)malloc((n + 1) * sizeof(char));
+	}
+}
+
+void print(maze_t *maze, stack_arr_t *s) {
+	int i;
+	for (i = 0; i < s->top; i++) {
+		printf("(%d, %d)", s->head[i].x+1, maze->m - s->head[i].y);
+		if(i != s->top - 1) {
+			printf("->");
+		}
 	}
 	printf("\n");
 }
-
-int next(maze_t *maze, list_t *l, int pos_x, int pos_y) {
-	if(pos_x - 1 >= 0 && maze.maze[pos_x - 1][pos_y] == '0') return next(matrix, l, pos_x - 1, pos_y) == 0)
+int next(maze_t *maze, stack_arr_t *l, int pos_x, int pos_y) {
+	// printf("(%d, %d) - (%d, %d) - %c\n", pos_x+1, maze->m - pos_y, pos_x, pos_y, maze->maze[pos_y][pos_x]);
+	// printf("top: %d, %d\n", top(l).x, top(l).y);
+	if(pos_x == (maze->n - 1) && pos_y == 0) {
+		push(pos_x, pos_y, l);
+		print(maze, l);
+	}
+	else {
+		if((pos_x - 1) >= 0 && maze->maze[pos_y][pos_x - 1] == '0' && !coord_exists(l, pos_x - 1, pos_y)) {
+			push(pos_x, pos_y, l);
+			next(maze, l, pos_x - 1, pos_y);
+			pop(l);
+		}
+		if((pos_y + 1) < maze->m && maze->maze[pos_y + 1][pos_x] == '0' && !coord_exists(l, pos_x, pos_y + 1)) {
+			push(pos_x, pos_y, l);
+			next(maze, l, pos_x, pos_y + 1);
+			pop(l);
+		}
+		if((pos_x + 1) < maze->n && maze->maze[pos_y][pos_x + 1] == '0' && !coord_exists(l, pos_x + 1, pos_y)) {
+			push(pos_x, pos_y, l);
+            next(maze, l, pos_x + 1, pos_y);
+			pop(l);
+		}
+		if((pos_y - 1) >= 0 && maze->maze[pos_y - 1][pos_x] == '0' && !coord_exists(l, pos_x, pos_y - 1)) {
+			push(pos_x, pos_y, l);
+			next(maze, l, pos_x, pos_y - 1);
+			pop(l);
+		}
+	}
 }
 
 int main(void) {
 	int m, n;
 	scanf("%d %d\n", &m, &n);
 	maze_t *maze = (maze_t *)malloc(sizeof(maze_t));
-	maze->m = m;
-	maze->n = n;
-	char** matrix = (char**)malloc(m * sizeof(char*));
-	maze->maze = matrix;
+	maze_init(maze, m, n);
 	for (int i = 0; i < m; i++) {
-		matrix[i] = (char*)malloc(n * sizeof(char));
 		for (int j = 0; j < n; j++) {
 			char temp;	
 			do {
 				scanf("%c", &temp);
 			}
 			while(temp == '\n');
-			matrix[i][j] = temp;
+			maze->maze[i][j] = temp;
 		}
 	}
 
-	int pos_x = 0, pos_y = 0;
-	list_t *list = (list_t*)malloc(sizeof(list_t));
-	next(matrix, list, pos_x, pos_y);
+	stack_arr_t *list = (stack_arr_t*)malloc(sizeof(stack_arr_t));
+	stack_init(list, m*n);
+	next(maze, list, 0, m - 1);
   return 0;
 }

ff85355171728eba277096b2f6c2cfda0bde3c84
ImJustChew <chewtzihwee@gmail.com>
2022-12-08 01:00:46 +0000

    Lec 12

---
+++ 13747 - EE2310_Lec_12_2.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+#ifndef  FRACTION_H
+#define  FRACTION_H
+
+class Fraction {
+public:
+	void input();
+	void display();         // display fraction
+	friend Fraction operator +(Fraction frac1, Fraction frac2);  
+	friend Fraction operator -(Fraction frac1, Fraction frac2);  
+	friend Fraction operator *(Fraction frac1, Fraction frac2);  
+	friend Fraction operator /(Fraction frac1, Fraction frac2);  
+
+private:
+	void reduce();       
+  	/* reduce the fraction to simplest form */
+	int numer, denom;
+};
+
+#endif
+
+void Fraction::input() {
+	char temp;
+	cin >> numer >> temp >> denom;
+}
+void Fraction::display(){
+	cout << numer << "/" << denom;
+}
+Fraction operator +(Fraction frac1, Fraction frac2) {
+    Fraction ans;
+	ans.numer = frac1.numer*frac2.denom + frac2.numer*frac1.denom;
+	ans.denom = frac1.denom*frac2.denom;
+	ans.reduce();
+    return ans;
+}
+
+Fraction operator -(Fraction frac1, Fraction frac2) {
+    Fraction ans;
+	ans.numer = frac1.numer*frac2.denom - frac2.numer*frac1.denom;
+	ans.denom = frac1.denom*frac2.denom;
+	ans.reduce();
+    return ans;
+}
+Fraction operator *(Fraction frac1, Fraction frac2){
+    Fraction ans;
+	ans.numer = frac1.numer*frac2.numer;
+	ans.denom = frac1.denom*frac2.denom;
+	ans.reduce();
+    return ans;
+} 
+Fraction operator /(Fraction frac1, Fraction frac2){
+	if(frac1.denom == 0 || frac2.numer == 0 || frac2.denom == 0) {
+		cout << "dividing by zero" << endl;
+		exit(0);
+	}
+    Fraction ans;
+	ans.numer = frac1.numer*frac2.denom;
+	ans.denom = frac1.denom*frac2.numer;
+	ans.reduce();
+    return ans;
+} 
+
+void Fraction::reduce(){
+	int gcdval;
+	do {
+		gcdval = __gcd(abs(numer), abs(denom));
+		if(gcdval != 0) {
+			numer /= gcdval;
+			denom /= gcdval;
+		}
+	}
+	while(gcdval > 1);
+}
+
+// DO NOT CHANGE main() !!!
+int main() {
+    Fraction num1, num2, ans;
+    char oper;
+    num1.input();
+    cin >> oper;
+    num2.input();
+
+    switch (oper) {
+      case '+':
+        ans = num1 + num2; break;
+      case '-':
+        ans = num1 - num2; break;
+      case '*':
+        ans = num1 * num2; break;
+      case '/':
+        ans = num1 / num2; break;
+    }
+
+    // display result
+    cout << '(';
+    num1.display();
+    cout << ") "  << oper << " (";
+    num2.display();
+    cout << ") = ";
+    ans.display();
+    cout << endl;
+
+    return 0;
+}
